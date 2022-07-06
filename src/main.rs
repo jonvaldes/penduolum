485baf8e27f9bad61991ed32533ea32054ccd55a
Jon Valdés <jon@valdes.cc>
2022-07-06 04:26:28 +0200

    Firt version

---
+++ src/main.rs
@@ -0,0 +1,196 @@
+use anyhow::Result;
+use anyhow::anyhow;
+use bytemuck::NoUninit;
+use notan::egui::{self, *};
+use notan::prelude::*;
+
+#[derive(AppState)]
+struct State {
+    clear_options: ClearOptions,
+    pipeline: Pipeline,
+    must_reload_shaders: bool,
+    frame_idx: usize,
+    constant_buffer: Buffer,
+    cb_data : CB,
+}
+
+
+#[repr(C)]
+#[derive(Default, Copy, Clone, NoUninit)]
+struct CB {
+    point_count: u32,
+
+	radius0: f32,
+ 	initial_phase0: f32,
+ 	period0: f32,
+ 	initial_amplitude0: f32,
+ 	amplitude_decay0: f32,
+
+	radius1: f32,
+ 	initial_phase1: f32,
+ 	period1: f32,
+ 	initial_amplitude1: f32,
+ 	amplitude_decay1: f32,
+}
+
+impl CB{
+    fn as_float_slice(&self) -> &[f32] {
+        bytemuck::cast_slice(bytemuck::bytes_of(self))
+    }
+}
+
+#[notan_main]
+fn main() -> Result<(), String> {
+    notan::init_with(setup)
+        .add_config(EguiConfig)
+        .add_config(WindowConfig::new()
+            .vsync()
+            .lazy_loop()
+            .resizable()
+            .multisampling(8))
+        .update(update)
+        .draw(draw).build()
+}
+
+fn load_pipeline(gfx: &mut Graphics) -> Result<Pipeline> {
+    let pipeline = gfx
+        .create_pipeline()
+        .from_raw(
+            &std::fs::read("src/shader.vert")?,
+            &std::fs::read("src/shader.frag")?)
+        .build().map_err(|e| anyhow!("Error: {}",e))?;
+    Ok(pipeline)
+}
+
+fn setup(gfx: &mut Graphics) -> State {
+    let clear_options = ClearOptions::color(Color::new(0.1, 0.2, 0.3, 1.0));
+
+    let pipeline = match load_pipeline(gfx) {
+        Ok(pipeline) => pipeline,
+        Err(err) => {
+            eprintln!("Error compiling shaders: {}", err);
+            panic!();
+        }
+    };
+
+    let cb: CB = Default::default();
+
+    let constant_buffer = gfx
+        .create_uniform_buffer(0, "CB")
+        .with_data(cb.as_float_slice())
+        .build()
+        .unwrap();
+
+
+    let cb_data = CB{
+        point_count: 30000,
+
+	    radius0: 0.4,
+ 	    initial_phase0: 1.5,
+ 	    period0: 30.0,
+ 	    initial_amplitude0: 2.5,
+ 	    amplitude_decay0: 0.99,
+
+	    radius1: 0.5,
+ 	    initial_phase1: 0.5,
+ 	    period1: 16.0,
+ 	    initial_amplitude1: 1.3,
+ 	    amplitude_decay1: 0.99,
+    };
+
+
+    State {
+        clear_options,
+        pipeline,
+        must_reload_shaders: false,
+        frame_idx: 0,
+        constant_buffer,
+        cb_data,
+    }
+}
+
+fn update(app: &mut App, state: &mut State) {
+    if app.keyboard.was_pressed(KeyCode::Escape) {
+        app.exit();
+    }
+    if state.frame_idx % 60 == 0 {
+        state.must_reload_shaders = true;
+    }
+    state.frame_idx +=1;
+}
+
+fn draw(gfx: &mut Graphics, plugins: &mut Plugins, state: &mut State) {
+
+    if state.must_reload_shaders {
+        state.must_reload_shaders = false;
+
+        match load_pipeline(gfx) {
+            Ok(pipeline) => {
+                println!("Shaders reloaded");
+                state.pipeline = pipeline;
+            },
+            Err(err) => {
+                eprintln!("Error compiling shaders: {}", err);
+            }
+        };
+    }
+
+
+    let mut renderer = gfx.create_renderer();
+
+
+    gfx.set_buffer_data( &state.constant_buffer, state.cb_data.as_float_slice());
+
+    renderer.begin(Some(&state.clear_options));
+    renderer.set_pipeline(&state.pipeline);
+    renderer.set_primitive(DrawPrimitive::TriangleStrip);
+    renderer.draw(0, state.cb_data.point_count as i32);
+    renderer.end();
+
+
+    let mut output = plugins.egui(|ctx| {
+        egui::SidePanel::left("side_panel").show(&ctx, |ui| {
+            use std::f32::consts::TAU;
+
+            ui.heading("Penduolum");
+            
+            ui.label("Point count");
+            ui.add(egui::Slider::new(&mut state.cb_data.point_count, 1000..=100000));
+
+            ui.label("Radius0");
+            ui.add(egui::Slider::new(&mut state.cb_data.radius0, 0.0..=0.7).suffix("°"));
+            ui.label("InitialPhase0");
+            ui.add(egui::Slider::new(&mut state.cb_data.initial_phase0, 0.0..=TAU).suffix("°"));
+            
+            ui.label("Period0");
+            ui.add(egui::Slider::new(&mut state.cb_data.period0, 0.0..=200.0).suffix("°"));
+           
+            ui.label("InitialAmplitude0");
+            ui.add(egui::Slider::new(&mut state.cb_data.initial_amplitude0, 0.0..=TAU).suffix("°"));
+           
+            ui.label("AplitudeDecay0");
+            ui.add(egui::Slider::new(&mut state.cb_data.amplitude_decay0, 0.9..=1.0).suffix("°"));
+           
+
+            ui.label("Radius1");
+            ui.add(egui::Slider::new(&mut state.cb_data.radius1, 0.0..=0.7).suffix("°"));
+            ui.label("InitialPhase1");
+            ui.add(egui::Slider::new(&mut state.cb_data.initial_phase1, 0.0..=TAU).suffix("°"));
+            
+            ui.label("Period1");
+            ui.add(egui::Slider::new(&mut state.cb_data.period1, 0.0..=200.0).suffix("°"));
+           
+            ui.label("InitialAmplitude1");
+            ui.add(egui::Slider::new(&mut state.cb_data.initial_amplitude1, 0.0..=TAU).suffix("°"));
+           
+            ui.label("AplitudeDecay1");
+            ui.add(egui::Slider::new(&mut state.cb_data.amplitude_decay1, 0.9..=1.0).suffix("°"));
+           
+           
+           
+            ui.separator();
+        });
+    });
+    gfx.render(&renderer);
+    gfx.render(&output);
+}
---
+++ src/shader.vert
@@ -0,0 +1,99 @@
+#version 450
+precision highp float;
+precision highp int;
+layout(location = 0) out vec3 v_color;
+
+
+layout(std140) uniform CB{
+	uint point_count;
+
+	float radius0;
+ 	float initial_phase0;
+ 	float period0;
+ 	float initial_amplitude0;
+ 	float amplitude_decay0;
+
+	float radius1;
+ 	float initial_phase1;
+ 	float period1;
+ 	float initial_amplitude1;
+ 	float amplitude_decay1;
+};
+
+
+#define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062
+
+vec2 pendulum(float radius,
+ 			  float initial_phase,
+ 			  float period,
+ 			  float initial_amplitude,
+ 			  float amplitude_decay,
+ 			  float t) {
+ 	float tp = t;
+
+	// The pendulum moves as a sine wave within
+	float base_movement = sin(initial_phase + tp * period);
+
+	float amplitude = initial_amplitude * pow(amplitude_decay, t);
+	
+	float current_angle = PI * 1.5 + amplitude * base_movement;
+
+	return radius * vec2(
+		cos(current_angle),
+		sin(current_angle));
+}
+
+vec2 pointPos(uint index) {
+	float t = index * 0.0001;
+	vec2 p0 = pendulum(
+			radius0,
+		 	initial_phase0,
+			period0,
+		 	initial_amplitude0,
+			amplitude_decay0,
+			t);
+
+	vec2 p1 = pendulum(
+			radius1,
+		 	initial_phase1,
+			period1,
+		 	initial_amplitude1,
+			amplitude_decay1,
+			t);
+
+	return p0 + p1;
+		
+}
+
+void main() {
+
+    uint rectIndex = gl_VertexID / 2;
+    uint localIndex = gl_VertexID % 2;
+
+    /*
+         *      0--1 3
+         *      | / /|
+         *      2/ 4-5
+         */
+    uint localCenterIndex = rectIndex + (localIndex & 1);
+    float lateralOffset = 0.001; 
+    if (localIndex == 1){
+        lateralOffset *= -1;
+    }
+
+    vec2 localPos = pointPos(localCenterIndex);
+    vec2 nextPos = pointPos(localCenterIndex+1);
+    vec2 tangent = normalize(nextPos - localPos);
+    vec2 perpDir = vec2(-tangent.y, tangent.x);
+
+    vec2 vertPos = localPos + perpDir * lateralOffset;
+
+	float color_t = gl_VertexID / float(point_count);
+
+    v_color = vec3(
+    		0.5 + 0.5 * sin(color_t * 2.0*PI),
+    		0.5 + 0.5 * cos(color_t * 2.0*PI),
+    		1.0);
+
+    gl_Position = vec4(vertPos, 0.0, 1.0);
+}
